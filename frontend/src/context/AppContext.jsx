057b4221384a196970c42964b64abbdfa5fe57e3
Annas Khan <annasking601@gmail.com>
2025-08-25 11:29:56 +0500

    Context Code

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,12 +1,23 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
-export const AppProvider = ({ children }) => {
-    const value = {};
+export const AppContextProvider = ({ children }) => {
+    // You can add state and functions to the context value here
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+
+    const value = {navigate, user, setUser, isSeller, setIsSeller};
+    
     return (
-        <AppContext.Provider value={{}}>
+        <AppContext.Provider value={value}>
             {children}
         </AppContext.Provider>
     );
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext);
 }
